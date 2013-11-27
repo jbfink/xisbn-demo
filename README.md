c0554b9b6da5ed8d77f2dc22ad77d5d8ca5dcc03
jbfink <john.fink@gmail.com>
2013-11-27 10:59:57 -0500

    Some initial fleshing out.

---
+++ README.md
@@ -0,0 +1,2 @@
+Eventually this will be a small Sinatra app to put in an OCLC xisbn and have it spit JSON out.
+
---
+++ app.rb
@@ -0,0 +1,17 @@
+require "sinatra"
+#require "sinatra/activerecord"
+
+#set :database, "sqlite3:///crm.db"
+
+#class Patron < ActiveRecord::Base
+#end
+
+helpers do
+	def xisbn_fetch(xisbn)
+	end
+end
+
+
+get '/' do
+	haml :"xisbn/index"
+end

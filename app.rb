2b5ffec7f3cc823dd6ccde5383d94366db76a865
jbfink <john.fink@gmail.com>
2013-11-28 14:13:36 -0500

    first attempt at retrieving json works but is ugly.

--- app.rb
+++ app.rb
@@ -1,7 +1,12 @@
 require "sinatra"
+require "open-uri"
+require "json"
+
 
 helpers do
 	def xisbn_fetch(xisbn)
+		item = JSON.load(open("http://xisbn.worldcat.org/webservices/xid/isbn/#{xisbn}?method=getEditions&format=json&fl=form,year,lang,ed"))
+		return JSON.pretty_generate(item)
 		return "You inputted #{xisbn}! Good for you."
 	end
 end

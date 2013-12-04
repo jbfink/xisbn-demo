d088265debee5b03256081a321488cd6bd5bf382
jbfink <john.fink@gmail.com>
2013-12-03 20:29:54 -0500

    this is not quite working but I wanted to commit so I could save my place.

--- app.rb
+++ app.rb
@@ -6,7 +6,9 @@ require "json"
 helpers do
 	def xisbn_fetch(xisbn)
 		item = JSON.load(open("http://xisbn.worldcat.org/webservices/xid/isbn/#{xisbn}?method=getEditions&format=json&fl=form,year,lang,ed"))
-		return JSON.pretty_generate(item)
+		item["list"].each do |k|
+			puts k["isbn"]
+		end
 		return "You inputted #{xisbn}! Good for you."
 	end
 end
